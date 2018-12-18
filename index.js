66f4e20efcece9e8916a11ceb2faa49315c0bf9b
Dominik <thepressu@gmail.com>
2018-12-18 10:20:49 +0100

    Add support for nicknames.

--- index.js
+++ index.js
@@ -10,25 +10,36 @@ http.listen(3000, function () {
 });
 
 
+var nicknames = {};
 
 var io = require('socket.io')(http);
-
 io.on('connection', function (socket) {
   console.log(`user connected: ${socket.id}`);
+  nicknames[socket.id] = socket.id;
   io.emit('user connected', socket.id);
+  io.emit('who is online', nicknames);
   socket.on('disconnect', function(){
     console.log(`user disconnected:  ${socket.id}`);
+    delete nicknames[socket.id];
     io.emit('user disconnected', socket.id);
+    io.emit('who is online', nicknames);
   });
   socket.on('chat message', function(msg){
-    console.log(`sent message by ${socket.id} : ${msg}`);
-    io.emit('chat message', msg);
+    console.log(`sent message by ${socket.id} (${nicknames[socket.id]}) : ${msg}`);
+    io.emit('chat message', [nicknames[socket.id], msg]);
   });
 });
 
 
-var nsp = io.of('/my-namespace');
+var nsp = io.of('/chat');
 nsp.on('connection', function(socket){
-  console.log('someone connected to namespace');
-  nsp.emit('hi', 'everyone!');
+  console.log('someone connected to chat');
+  nsp.emit('hi', 'You are connected to chat successfully!');
+  socket.on('nick change', function(nick){
+    let id = socket.id.substr(socket.id.indexOf('#')+1);
+    nicknames[id] = nick;
+    console.log(nicknames);
+    console.log(`user ${socket.id} changed nick: ${nick}`);
+    nsp.emit('who is online', nicknames);
+  });
 });
--- readme.txt
+++ readme.txt
@@ -1,4 +1,3 @@
-Add support for nicknames.
 Don’t send the same message to the user that sent it himself. Instead, append the message directly as soon as he presses enter.
 Add “{user} is typing” functionality.
 Show who’s online.
