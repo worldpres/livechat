25f5d6dcbdc87daf036713faeaab01856f62a4d8
Dominik <thepressu@gmail.com>
2018-12-18 08:15:52 +0100

    socket works - connection

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 var app = require('express')();
 var http = require('http').Server(app);
 
-app.get('/', function(req, res){
-//   res.send('<h1>Hello world</h1>');
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function(){
+http.listen(3000, function () {
   console.log('listening on *:3000');
+});
+
+
+
+var io = require('socket.io')(http);
+
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
