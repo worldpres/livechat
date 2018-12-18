795fa6f7cc08000befddf9e192e6f63a9445f890
Dominik <thepressu@gmail.com>
2018-12-18 12:43:27 +0100

    Add “{user} is typing” functionality.

--- index.js
+++ index.js
@@ -19,29 +19,43 @@ io.on('connection', function (socket) {
   nicknames[socket.id] = socket.id;
   io.emit('user connected', socket.id);
   io.emit('who is online', nicknames);
-  socket.on('disconnect', function(){
+  socket.on('disconnect', function () {
     console.log(`user disconnected:  ${socket.id}`);
     delete nicknames[socket.id];
     io.emit('user disconnected', socket.id);
     io.emit('who is online', nicknames);
   });
-  socket.on('chat message', function(msg){
+  socket.on('chat message', function (msg) {
     console.log(`sent message by ${socket.id} (${nicknames[socket.id]}) : ${msg}`);
     let date = new Date().toLocaleString('pl-PL');
     io.emit('chat message', [nicknames[socket.id], date, msg]);
   });
 });
 
+var writers = [];
 
 var nsp = io.of('/chat');
-nsp.on('connection', function(socket){
+nsp.on('connection', function (socket) {
   console.log('someone connected to chat');
   nsp.emit('hi', 'You are connected to chat successfully!');
-  socket.on('nick change', function(nick){
-    let id = socket.id.substr(socket.id.indexOf('#')+1);
+  socket.on('nick change', function (nick) {
+    let id = socket.id.substr(socket.id.indexOf('#') + 1);
     nicknames[id] = nick;
     console.log(nicknames);
     console.log(`user ${socket.id} changed nick: ${nick}`);
     nsp.emit('who is online', nicknames);
   });
+  socket.on('im writing', function (write) {
+    let id = socket.id.substr(socket.id.indexOf('#') + 1);
+    if (write) {
+      if (writers.indexOf(id) == -1) writers.push(id);
+    }
+    if (!write) {
+      if (writers.indexOf(id) != -1) writers.splice(writers.indexOf(id), 1);
+    }
+    console.log(writers.map(v => nicknames[v]));
+    // console.log(`user ${socket.id} write something: ${write}`);
+    nsp.emit('writers', writers.map(v => nicknames[v]));
+  });
+
 });
--- readme.txt
+++ readme.txt
@@ -1,4 +1,1 @@
-Don’t send the same message to the user that sent it himself. Instead, append the message directly as soon as he presses enter.
-Add “{user} is typing” functionality.
-Show who’s online.
 Add private messaging.
