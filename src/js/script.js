504b12c3dbadd912795bceae2744cc3c55f135d3
Dominik <thepressu@gmail.com>
2019-01-06 14:15:57 +0100

    vue for footer (year for copyright)

--- src/js/script.js
+++ src/js/script.js
@@ -3,9 +3,16 @@ $(() => {
     $('.modal').modal();
 
     vueAppMain = new Vue({
+        el: '#footer',
+        data: {
+            copyYear: new Date().getFullYear(),
+        }
+    });
+
+    vueAppMain = new Vue({
         el: '#vue-app-main',
         data: {
             onlineUsers: ``,
         }
-    })
+    });
 });
